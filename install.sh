bdba0adbbf6f89a26f39309058220148ffbf9285
andreyctkn <andrey.mykhaliuk@gmail.com>
2023-11-29 20:34:20 -0800

    fix logs

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 [ "${SHELL##/*/}" != "zsh" ] && echo 'You might need to change default shell to zsh: `chsh -s /bin/zsh`'
 
-echo "[Repo Init]: ⏳ \n"
+echo "\n[Repo Init]: ⏳ \n"
 
 export DEV_DIR="$HOME/Dev"
 mkdir -p $DEV_DIR
@@ -10,17 +10,17 @@ cd $DEV_DIR
 git clone --recursive https://github.com/andreyctkn/dotfiles.git
 cd dotfiles
 
-echo "[Repo Init]: 👌 \n"
+echo "\n[Repo Init]: 👌 \n"
 
 echo "[Symlinking config files]: ⏳ \n"
 sh etc/symlink-dotfiles.sh
-echo "[Symlinking config files]: 👌 \n"
+echo "\n[Symlinking config files]: 👌 \n"
 
-echo "[brew install & openjdk & nodejs]: ⏳ \n"
+echo "\n[brew install & openjdk & nodejs]: ⏳ \n"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install node
 # Install JVM via https://gist.github.com/gwpantazes/50810d5635fc2e053ad117b39b597a14 
 brew install openjdk
 sudo ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
-echo "[brew install & openjdk & nodejs]: 👌 \n"
+echo "\n[brew install & openjdk & nodejs]: 👌 \n"
